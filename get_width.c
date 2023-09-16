9c2440c98c1c4d2d8904edc9f03f1ea0ebd7f358
kachisapphire <138621952+kachisapphire@users.noreply.github.com>
2023-09-16 19:54:17 +0100

    Update get_width.c
    printf group project

--- get_width.c
+++ get_width.c
@@ -1,32 +1,32 @@
 #include "main.h"
-  2 /**
-  3  *get_width - Calculates the width for printing
-  4  *@format: Formatted string in which to print the arguments.
-  5  *@i: List of arguments to be printed.
-  6  *@list: list of arguments.
-  7  *Return: width.
-  8  */
-  9 int get_width(const char *format, int *i, va_list list)
- 10 {
- 11         int curr_i;
- 12         int width = 0;
- 13 
- 14         for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
- 15         {
- 16                 if (is_digit(format[curr_i]))
- 17                 {
- 18                         width *= 10;
- 19                         width += format[curr_i] - '0';
- 20                 }
- 21                 else if (format[curr_i] == '*')
- 22                 {
- 23                         curr_i++;
- 24                         width = va_arg(list, int);
- 25                         break;
- 26                 }
- 27                 else
- 28                         break;
- 29         }
- 30         *i = curr_i - 1;
- 31         return (width);
- 32 }
+   /**
+    *get_width - Calculates the width for printing
+    *@format: Formatted string in which to print the arguments.
+    *@i: List of arguments to be printed.
+    *@list: list of arguments.
+    *Return: width.
+    */
+   int get_width(const char *format, int *i, va_list list)
+  {
+          int curr_i;
+          int width = 0;
+  
+          for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+          {
+                  if (is_digit(format[curr_i]))
+                  {
+                          width *= 10;
+                          width += format[curr_i] - '0';
+                  }
+                  else if (format[curr_i] == '*')
+                  {
+                          curr_i++;
+                          width = va_arg(list, int);
+                          break;
+                  }
+                  else
+                          break;
+          }
+          *i = curr_i - 1;
+          return (width);
+  }
